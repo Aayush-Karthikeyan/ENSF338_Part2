d09398900c6dbe9931bdc8cc44d33adc18ecce5f
Dave <davidgb4005@gmail.com>
2026-03-04 11:23:04 -0700

    Task Finished

--- Task.java
+++ Task.java
@@ -3,7 +3,14 @@ package edu.ucalgary.oop;
 import java.util.Objects;
 
 public class Task {
+    private String id;
+    private String title;
+    private boolean isCompleted = false;
 
+    public Task(String id,String title){
+        this.id = id;
+        this.title = title;
+    }
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -13,7 +20,9 @@ public class Task {
                Objects.equals(title, task.title) &&
                isCompleted == task.isCompleted; 
     }
-
+    public Task copy(Task task){
+        return(new Task(this.id,this.title));
+    }
     @Override
     public int hashCode() {
         return Objects.hash(id, title, isCompleted);
